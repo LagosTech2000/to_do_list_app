158e90d7d8aba642ee691cf0d720521b6330a98e
Ferando Lagos <Fernandolagos2016@gmail.com>
2024-05-04 15:25:34 -0600

    added user management with devise, tasks scaffold, imported tailwind cdn, styled header, new and index views

---
+++ db/migrate/20240504040534_create_tasks.rb
@@ -0,0 +1,11 @@
+class CreateTasks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.text :description
+      t.datetime :dueDate
+
+      t.timestamps
+    end
+  end
+end
